e3ab7fec28b86b3fe9c7fc60d85262ea0618ee6c
Rencheng Chua <rench00@gmail.com>
2016-10-20 11:06:58 +0800

    clean up code

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,3 @@
-// $('#uploadFile').change(function(e){
-//   var file = e.target.files[0];
-//   console.log("the file is ", file);
-//
-//   var formData = new FormData();
-//   formData.append('uploadFile', file);
-//   console.log(formData);
-//
-//   // post
-//   $.ajax({
-//     url:'http://localhost:1337/csv',
-//     type:'post',
-//     data: formData,
-//     processData: false,
-//     contentType: false,
-//     success: function (data, status, jqXHR){
-//       console.log(data);
-//     },
-//     crossDomain: true
-//   })
-// })
-
 import React from "react";
 import ReactDOM from "react-dom";
 
